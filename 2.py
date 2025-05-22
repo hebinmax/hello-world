60f6236756928ec2ce9172b3499ab381b69d2f1d
hebin <664126680@qqcom>
2025-05-22 18:07:06 +0800

    logs

--- 2.py
+++ 2.py
@@ -3,25 +3,3 @@ import altair as alt
 import pandas as pd
 import numpy as np
 st.title(':red[Hello], :blue[World]!')
-st.header('sdfjksdfj :green[cool] :balloon:',help='This is a help text.',  anchor=False)
-
-st.caption("This is a string that explains something above.")
-st.caption("A caption with _italics_ :blue[colors] and emojis :sunglasses:")
-st.markdown("*Streamlit* is **really** ***cool***.")
-st.markdown('''
-    :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
-    :gray[pretty] :rainbow[colors] and :blue-background[highlight] text.''')
-st.markdown("Here's a bouquet &mdash;\
-            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
-
-
-md = st.text_area('Type in your markdown string (without outer quotes)',
-                  "Happy Streamlit-ing! :balloon:")
-
-st.code(f"""
-import streamlit as st
-
-st.markdown('''{md}''')
-""")
-
-st.markdown(md)
