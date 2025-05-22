b4c569b80577df25864cfa48be3ad4185eb71dc1
hebinmax <664126680@qq.com>
2025-05-22 17:10:30 +0800

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import altair as alt
+import pandas as pd
+import numpy as np
+
+st.header('sdfjksdfj :green[cool] :balloon:',help='This is a help text.',  anchor=False)
+
+st.caption("This is a string that explains something above.")
+st.caption("A caption with _italics_ :blue[colors] and emojis :sunglasses:")
+st.markdown("*Streamlit* is **really** ***cool***.")
+st.markdown('''
+    :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
+    :gray[pretty] :rainbow[colors] and :blue-background[highlight] text.''')
+st.markdown("Here's a bouquet &mdash;\
+            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
+
+
+md = st.text_area('Type in your markdown string (without outer quotes)',
+                  "Happy Streamlit-ing! :balloon:")
+
+st.code(f"""
+import streamlit as st
+
+st.markdown('''{md}''')
+""")
+
+st.markdown(md)
