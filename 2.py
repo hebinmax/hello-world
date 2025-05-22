0f9f2cf6ffc7621d15358359a64ab4f0232295c5
hebin <664126680@qqcom>
2025-05-22 20:29:16 +0800

    logs

--- 2.py
+++ 2.py
@@ -2,4 +2,10 @@ import streamlit as st
 import altair as alt
 import pandas as pd
 import numpy as np
+from datetime import time, datetime
 st.title(':red[Hello], :blue[World]!')
+st.header('st.slider')
+st.subheader('slider')
+age = st.slider('How old are you?', 0, 130, 25)
+
+
